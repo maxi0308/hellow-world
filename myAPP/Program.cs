f31d3642a521298226a1ae6442d38d5e707ae943
maxi0308 <33596329+maxi0308@users.noreply.github.com>
2018-02-25 13:51:28 +0500

    Add files via upload

---
+++ myAPP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace myAPP {
+    class Program {
+        static void Main (string[] args) {
+                      string secondMessage = " my name is aftab";
+           
+            Console.WriteLine ($"hellow{secondMessage} and my this string has {secondMessage.length} alphabets  ");
+          
+        }
+    }
+}
